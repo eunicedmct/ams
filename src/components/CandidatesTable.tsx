a28a65eff567c63f2239b77d10cae31309286899
rm-eunicedmct <rm.eunicedmct@gmail.com>
2021-11-29 18:32:06 +0800

    Fix console errors.

--- src/components/CandidatesTable.tsx
+++ src/components/CandidatesTable.tsx
@@ -10,28 +10,32 @@ function CandidatesTable({ candidates }: Props) {
   return (
     <div className="CandidatesTable">
       <table>
-        <tr>
-          <th>Name</th>
-          <th>Age</th>
-          <th>Email</th>
-          <th>Interview Score</th>
-          <th>Technical Score</th>
-          <th>Application Date</th>
-        </tr>
-        {
-          candidates.map((candidate) => {
-            return (
-              <tr key={candidate.email}>
-                <td>{candidate.first_name} {candidate.last_name}</td>
-                <td>{candidate.age}</td>
-                <td>{candidate.email}</td>
-                <td>{candidate.interview_score}</td>
-                <td>{candidate.technical_score}</td>
-                <td>{candidate.created_at.toLocaleString("en-US")}</td>
-              </tr>
-            )
-          })
-        }
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Email</th>
+            <th>Interview Score</th>
+            <th>Technical Score</th>
+            <th>Application Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+              candidates.map((candidate, idx) => {
+              return (
+                  <tr key={idx}>
+                  <td>{candidate.first_name} {candidate.last_name}</td>
+                  <td>{candidate.age}</td>
+                  <td>{candidate.email}</td>
+                  <td>{candidate.interview_score}</td>
+                  <td>{candidate.technical_score}</td>
+                  <td>{candidate.created_at.toLocaleString("en-US")}</td>
+                  </tr>
+              )
+              })
+            }
+        </tbody>
       </table>
     </div>
   );
